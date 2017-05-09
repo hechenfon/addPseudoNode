83f103abfed877f5e0ee995a40d32c2186d6ac37
hechenfon <hechenfon@utexas.edu>
2017-05-09 16:11:22 -0500

    added row clustering, fixed findAllMut bug

---
+++ src/pseudo/mutModel.java
@@ -0,0 +1,18 @@
+package pseudo;
+
+import java.util.HashMap;
+
+/**
+ * Created by hechenfon on 5/2/2017.
+ * This class is for different mutation models - BASELINe in R package 'SHAZAM'
+ * store the mutation probabilities(targeting) as multiple HashMap
+ */
+public class mutMode {
+    HashMap<String,HashMap<Character,Double>> modMat;
+
+    public mutMode (String filename) {
+        
+
+    }
+
+}
